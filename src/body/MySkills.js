54fed3ddec41e3361907312241004e66d68c2abd
piyush kumar <piyushkumar.as@krishnacollege.ac.in>
2021-08-14 12:31:13 +0530

    commit for git

--- src/body/MySkills.js
+++ src/body/MySkills.js
@@ -7,16 +7,16 @@ const MySkills = () => {
         if (data._id % 2 === 0) {
             return (
                 <div className="skill-row mt-4"  >
-                    <img style={{ maxHeight: '35%',minHeight:'33%',minWidth:"43%" ,maxWidth: '45%', float: "right", marginLeft: "30px" }} src={data.image} alt={data.name} />
-                    <h3 style={{ marginBottom: '3%'}}>{data.name}</h3>
+                    <img style={{ maxHeight: '35%', minHeight: '33%', minWidth: "43%", maxWidth: '45%', float: "right", marginLeft: "30px" }} src={data.image} alt={data.name} />
+                    <h3 style={{ marginBottom: '3%' }}>{data.name}</h3>
                     <p>{data.description}</p>
                 </div>
             );
         } else {
             return (
                 <div className="skill-row mt-4">
-                    <img style={{ maxHeight: '35%',minHeight:'33%',minWidth:"43%" ,maxWidth: '45%', float: "left", marginRight: "30px" }} src={data.image} alt={data.name} />
-                    <h3 style={{ marginBottom: '3%'}}>{data.name}</h3>
+                    <img style={{ maxHeight: '35%', minHeight: '33%', minWidth: "43%", maxWidth: '45%', float: "left", marginRight: "30px" }} src={data.image} alt={data.name} />
+                    <h3 style={{ marginBottom: '3%' }}>{data.name}</h3>
                     <p>{data.description}</p>
                 </div>
 
@@ -25,10 +25,10 @@ const MySkills = () => {
     };
 
     return (
-        <div id="myskills" style={{ marginBottom:'15%'}}>
-            <h2 className="my-skills" style={{marginBottom:"7%", fontSize: '155%'}}>My Skills.</h2>
+        <div id="myskills" style={{ marginBottom: '15%' }}>
+            <h2 className="my-skills" style={{ marginBottom: "7%", fontSize: '155%' }}>My Skills.</h2>
             {myskillsData.map((p, i) => (
-                <div key={i} style={{marginBottom :"10%"}}>
+                <div key={i} style={{ marginBottom: "10%" }}>
                     {leftRight(p)}
                 </div>
             ))}
